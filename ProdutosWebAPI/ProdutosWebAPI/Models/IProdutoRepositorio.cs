3151454647e9969a7ec20519595ebb40c26ef3e8
Rafael Miranda <rafael.miranda@db1.com.br>
2019-09-27 14:27:37 -0300

    [DEV] primeira aula - cração do projeto e de classes iniciais

---
+++ ProdutosWebAPI/ProdutosWebAPI/Models/IProdutoRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ProdutosWebAPI.Models
+{
+    public interface IProdutoRepositorio
+    {
+        IEnumerable<Produto> GetAll();
+        Produto Get(int id);
+        Produto Add(Produto item);
+        bool Update(Produto item);
+        void Remove(int id);
+    }
+}
---
+++ ProdutosWebAPI/ProdutosWebAPI/Repositorio/ProdutoRepositorio.cs
@@ -0,0 +1,64 @@
+﻿using ProdutosWebAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ProdutosWebAPI.Repositorio
+{
+    public class ProdutoRepositorio : IProdutoRepositorio
+    {
+        private List<Produto> produtos = new List<Produto>();
+        private int _nextId = 1;
+
+        public ProdutoRepositorio()
+        {
+            Add(new Produto { Nome = "Guaraná Antartica", Categoria = "Refrigerantes", Preco = 4.99M });
+            Add(new Produto { Nome = "Suco de Laranja Prats", Categoria = "Sucos", Preco = 5.68M });
+            Add(new Produto { Nome = "Mostarda Hammer", Categoria = "Condimentos", Preco = 3.90M });
+            Add(new Produto { Nome = "Molho de Tomate Elefante", Categoria = "Condimentos", Preco = 2.99M });
+            Add(new Produto { Nome = "Suco de Uva Prats", Categoria = "Sucos", Preco = 6.68M });
+        }
+
+        public Produto Add(Produto item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentException("item");
+            }
+
+            item.Id = _nextId++;
+            produtos.Add(item);
+            return item;
+        }
+
+        public Produto Get(int id)
+        {
+            return produtos.Find(p => p.Id == id);
+        }
+
+        public IEnumerable<Produto> GetAll()
+        {
+            return produtos;
+        }
+
+        public void Remove(int id)
+        {
+            produtos.RemoveAll(p => p.Id == id);
+        }
+
+        public bool Update(Produto item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentException("item");
+            }
+            var index = produtos.FindIndex(p => p.Id == item.Id);
+            if (index == -1)
+            {
+                return false;
+            }
+            produtos.RemoveAt(index);
+            produtos.Add(item);
+            return true;
+        }
+    }
+}
