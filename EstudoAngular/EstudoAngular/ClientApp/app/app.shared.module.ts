56eb1c6138f2bdf531ba05f5e0d5f98683124a08
Rafael Miranda <rafael.miranda@db1.com.br>
2019-10-01 15:42:54 -0300

    [DEV] primeiro componente - cliente

--- EstudoAngular/EstudoAngular/ClientApp/app/app.shared.module.ts
+++ EstudoAngular/EstudoAngular/ClientApp/app/app.shared.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        ClienteComponent
     ],
     imports: [
         CommonModule,
@@ -25,8 +27,8 @@ import { CounterComponent } from './components/counter/counter.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'clientes', component: ClienteComponent },
+            //{ path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
---
+++ EstudoAngular/EstudoAngular/ClientApp/app/components/Cliente/cliente.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cliente',
+    templateUrl: './cliente.component.html'
+})
+export class ClienteComponent {
+    
+}
