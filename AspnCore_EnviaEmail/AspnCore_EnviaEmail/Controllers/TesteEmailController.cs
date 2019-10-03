eeef09d96cb75a2c4c0f49b1cf63223c87bb7031
Rafael Miranda <rafael.miranda@db1.com.br>
2019-10-03 13:47:18 -0300

    [DEV] Enviar email

---
+++ AspnCore_EnviaEmail/AspnCore_EnviaEmail/Controllers/TesteEmailController.cs
@@ -0,0 +1,65 @@
+﻿using AspnCore_EnviaEmail.Models;
+using AspnCore_EnviaEmail.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace AspnCore_EnviaEmail.Controllers
+{
+    public class TesteEmailController : Controller
+    {
+        private readonly IEmailSender _emailSender;
+
+        public TesteEmailController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        //get
+        public IActionResult EnviarEmail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EnviarEmail(EmailModel email)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    TesteEnvioEmail(email.Destino, email.Assunto, email.Mensagem).GetAwaiter();
+                    return RedirectToAction("EmailEnviado");
+                }
+                catch (Exception)
+                {
+                    return RedirectToAction("EmailFalhou");
+                }
+            }
+            return View(email);
+        }
+
+        public async Task TesteEnvioEmail(string email, string assunto, string mensagem)
+        {
+            try
+            {
+                //email destino, assunto do email, mensagem a enviar
+                await _emailSender.SendEmailAsync(email, assunto, mensagem);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public ActionResult EmailEnviado()
+        {
+            return View();
+        }
+
+        public ActionResult EmailFalhou()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspnCore_EnviaEmail/AspnCore_EnviaEmail/Services/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace AspnCore_EnviaEmail.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
